63b88e1a3ee2b29f2670632cf0c951c9b68e7c02
Alexandre P Francisco <aplf@ist.utl.pt>
2011-10-24 09:34:19 +0100

    Move files from local.

--- main.js
+++ main.js
@@ -21,14 +21,26 @@ console.log();
 
 console.log("Search: GAC");
 console.log(my_map.search('GAC'));
+console.log(my_map.fast_search('GAC'));
 console.log();
 
 console.log("Search: GA");
 console.log(my_map.search('GA'));
+console.log(my_map.fast_search('GA'));
 console.log();
 
 console.log("Search: A");
 console.log(my_map.search('A'));
+console.log(my_map.fast_search('A'));
 console.log();
 
+console.log(my_map.b2d(         0,          0, 0));
+console.log(my_map.b2d(         0, 1073741824, 1));
+console.log(my_map.b2d(1073741824,  536870912, 2));
+console.log(my_map.b2d( 536870912,  268435456, 3));
+console.log(my_map.b2d( 268435456, 1207959552, 4));
+console.log(my_map.b2d( 134217728, 1677721600, 5));
+console.log(my_map.b2d(1140850688,  838860800, 6));
+console.log(my_map.b2d( 570425344,  419430400, 7));
+console.log();
 
